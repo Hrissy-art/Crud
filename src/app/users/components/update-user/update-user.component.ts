357825be7f36285084ea188203d072a820e59418
Hristina <hristinastankova89@gmail.com>
2024-11-18 16:43:13 +0100

    update user method

--- src/app/users/components/update-user/update-user.component.ts
+++ src/app/users/components/update-user/update-user.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Component, inject, NgModule } from '@angular/core';
+import { Component, inject, Input, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../../models/message.model';
 import { ApiService } from '../services/api.service';
@@ -14,55 +14,10 @@ import { map, Observable, switchMap, tap } from 'rxjs';
   styleUrl: './update-user.component.css'
 })
 export class UpdateUserComponent{
-  userId$: Observable<string>; 
-  editableUser: User | null = null; 
-
-  private _apiService: ApiService = inject(ApiService);
-  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
-  private router: Router = inject(Router);
-
-  constructor() {
-    // Extraction de l'ID comme une chaîne
-    this.userId$ = this.activatedRoute.paramMap.pipe(
-      map(params => {
-        const userId = params.get('id') || ''; // ID en tant que chaîne
-        console.log('ID extrait de l\'URL:', userId);
-        return userId;
-      })
-    );
-
-    // Utilisation de l'ID comme une chaîne pour récupérer l'utilisateur
-    this.userId$.pipe(
-      switchMap(userId => 
-        this._apiService.getAllUsers$().pipe(
-          map(users => users.find(user => user.id === userId) || null),
-          tap(user => {
-            if (user) {
-              this.editableUser = { ...user };  // Crée une copie de l'utilisateur
-              console.log('Utilisateur chargé:', this.editableUser);
-            } else {
-              console.warn('Utilisateur non trouvé.');
-            }
-          })
-        )
-      )
-    ).subscribe();
-  }
+  private _userApiService: ApiService = inject(ApiService);
+  @Input({ required:true}) user!: User
 
   onSubmit(): void {
-    if (this.editableUser && this.editableUser.id) {
-      console.log('Envoi des données mises à jour:', this.editableUser);
-
-      // Passe l'ID comme chaîne
-      this._apiService.updateUser$(this.editableUser.id, this.editableUser).subscribe({
-        next: updatedUser => {
-          console.log('Utilisateur mis à jour avec succès:', updatedUser);
-          this.router.navigate(['/users']);  // Redirige vers la liste des utilisateurs
-        },
-        error: err => {
-          console.error('Erreur lors de la mise à jour:', err);
-        }
-      });
-    }
+    this._userApiService.updateUser$(this.user);
   }
 }
